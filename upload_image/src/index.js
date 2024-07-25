113106a7e9cda7c468cbe084a6717df927bcd057
Kyle Ramsy <kjramsy@gmail.com>
2024-07-25 15:53:55 -0500

    chore: Add @azure/storage-blob dependency for image upload functionality

--- upload_image/src/index.js
+++ upload_image/src/index.js
@@ -1,5 +1,34 @@
-const { app } = require('@azure/functions');
+const {
+    StorageSharedKeyCredential,
+    ContainerSASPermissions,
+    generateBlobSASQueryParameters
+} = require("@azure/storage-blob");
+const { extractConnectionStringParts } = require('./utils.js');
 
-app.setup({
-    enableHttpStream: true,
-});
+    module.exports = async function (context, req) {
+    const permissions = 'c';
+    const container = 'images';
+    context.res = {
+        body: generateSasToken(process.env.AzureWebJobsStorage, container, permissions)
+    };
+    context.done();
+};
+
+function generateSasToken(connectionString, container, permissions) {
+    const { accountKey, accountName, url } = extractConnectionStringParts(connectionString);
+    const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey.toString('base64'));
+
+    var expiryDate = new Date();
+    expiryDate.setHours(expiryDate.getHours() + 2);
+
+    const sasKey = generateBlobSASQueryParameters({
+        containerName: container,
+        permissions: ContainerSASPermissions.parse(permissions),
+        expiresOn: expiryDate,
+    }, sharedKeyCredential);
+
+    return {
+        sasKey: sasKey.toString(),
+        url: url
+    };
+}
